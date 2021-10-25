3101f2be987a877c3a7f4fa24707c5207b9bb03b
jomart-gsu <jmartin191@gsu.edu>
2021-10-25 11:25:05 -0400

    styling and other setup

--- app.py
+++ app.py
@@ -1,58 +1,203 @@
 import flask
 from flask_login.utils import login_required
+import requests
 import os
 import json
 
+import random
+import base64
+from dotenv import load_dotenv, find_dotenv
 
-app = flask.Flask(__name__, static_folder='./build/static')
-# This tells our Flask app to look at the results of `npm build` instead of the 
-# actual files in /templates when we're looking for the index page file. This allows
-# us to load React code into a webpage. Look up create-react-app for more reading on
-# why this is necessary.
+load_dotenv(find_dotenv())
+
+from genius import get_lyrics_link
+from spotify import get_access_token, get_song_data
+
+from flask_login import login_user, current_user, LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = flask.Flask(__name__, static_folder="./build/static")
+# Point SQLAlchemy to your Heroku database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+from flask_login import UserMixin
+
+db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def get_username(self):
+        return self.username
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<Artist {self.artist_id}>"
+
+
+db.create_all()
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_name):
+    return User.query.get(user_name)
+
+
+def get_access_token():
+    auth = base64.standard_b64encode(
+        bytes(
+            f"{os.getenv('SPOTIFY_CLIENT_ID')}:{os.getenv('SPOTIFY_CLIENT_SECRET')}",
+            "utf-8",
+        )
+    ).decode("utf-8")
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        headers={"Authorization": f"Basic {auth}"},
+        data={"grant_type": "client_credentials"},
+    )
+    json_response = response.json()
+    return json_response["access_token"]
+
+
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
-@bp.route('/index')
+
+@bp.route("/index")
 @login_required
 def index():
-    # TODO: insert the data fetched by your app main page here as a JSON
-    DATA = {"your": "data here"}
-    data = json.dumps(DATA)
+    artists = Artist.query.filter_by(username=current_user.username).all()
+    artist_ids = [a.artist_id for a in artists]
+    has_artists_saved = len(artist_ids) > 0
+    if has_artists_saved:
+        artist_id = random.choice(artist_ids)
+
+        # API calls
+        access_token = get_access_token()
+        (song_name, song_artist, song_image_url, preview_url) = get_song_data(
+            artist_id, access_token
+        )
+        genius_url = get_lyrics_link(song_name)
+
+    else:
+        (song_name, song_artist, song_image_url, preview_url, genius_url) = (
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+
+    data = json.dumps(
+        {
+            "username": current_user.username,
+            "artist_ids": artist_ids,
+            "has_artists_saved": has_artists_saved,
+            "song_name": song_name,
+            "song_artist": song_artist,
+            "song_image_url": song_image_url,
+            "preview_url": preview_url,
+            "genius_url": genius_url,
+        }
+    )
     return flask.render_template(
         "index.html",
         data=data,
     )
 
+
 app.register_blueprint(bp)
 
-@app.route('/signup')
+
+@app.route("/signup")
 def signup():
-	...
+    return flask.render_template("signup.html")
 
-@app.route('/signup', methods=["POST"])
+
+@app.route("/signup", methods=["POST"])
 def signup_post():
-	...
+    username = flask.request.form.get("username")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        pass
+    else:
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
 
-@app.route('/login')
+    return flask.redirect(flask.url_for("login"))
+
+
+@app.route("/login")
 def login():
-    ...
+    return flask.render_template("login.html")
 
-@app.route('/login', methods=["POST"])
+
+@app.route("/login", methods=["POST"])
 def login_post():
-	...
+    username = flask.request.form.get("username")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        login_user(user)
+        return flask.redirect(flask.url_for("bp.index"))
 
-@app.route('/save', methods=["POST"])
+    else:
+        return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
+
+
+@app.route("/save", methods=["POST"])
 def save():
-    ...
+    artist_ids = flask.request.json.get("artist_ids")
+    print(flask.request.json)
+    valid_ids = set()
+    for artist_id in artist_ids:
+        try:
+            access_token = get_access_token()
+            get_song_data(artist_id, access_token)
+            valid_ids.add(artist_id)
+        except Exception:
+            pass
+
+    username = current_user.username
+    existing_ids = {
+        v.artist_id for v in Artist.query.filter_by(username=username).all()
+    }
+    new_ids = valid_ids - existing_ids
+    for new_id in new_ids:
+        db.session.add(Artist(artist_id=new_id, username=username))
+    for artist in Artist.query.filter_by(username=username).filter(
+        Artist.artist_id.notin_(valid_ids)
+    ):
+        db.session.delete(artist)
+    db.session.commit()
+
+    response = {"artist_ids": [a for a in artist_ids if a in valid_ids]}
+    return flask.jsonify(response)
 
 
-@app.route('/')
+@app.route("/")
 def main():
-	...
-
-
+    if current_user.is_authenticated:
+        return flask.redirect(flask.url_for("bp.index"))
+    return flask.redirect(flask.url_for("login"))
 
 
 app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8081)),
+    host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", 8081)),
 )
--- src/App.js
+++ src/App.js
@@ -1,10 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 
 
 function App() {
-  // TODO: Implement your main page as a React component.
+  const args = JSON.parse(document.getElementById("data").text);
+  const [artists, updateArtists] = useState(args.artist_ids);
+  const form = useRef(null);
+
+  function onClickAdd() {
+    let val = form.current.value;
+    let updatedArtists = [...artists, val];
+    updateArtists(updatedArtists);
+    form.current.value = "";
+    console.log(updatedArtists);
+  }
+
+  function onClickDelete(i) {
+    let updatedArtists = [...artists.slice(0, i), ...artists.slice(i + 1)];
+    updateArtists(updatedArtists);
+  }
+
+  function onClickSave() {
+    let data = { "artist_ids": artists };
+    console.log(JSON.stringify(data))
+    fetch('/save', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data)
+        updateArtists(data.artist_ids);
+      });
+  }
+
+  const deleteButtonStyle = {
+    backgroundColor: 'red',
+    border: 'none',
+    color: 'white',
+    padding: '15px 32px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+  };
+
+  const gridStyle = {
+    align: 'center',
+    display: 'grid',
+    gridTemplateColumns: '2fr 1fr',
+    gridGap: '10px 5px',
+    marginLeft: '30%',
+    marginRight: '30%',
+  };
+
+  const artists_list = artists.map((artist_id, i) => (
+    <div style={gridStyle}>
+      <p>{artist_id}</p>
+      <button style={deleteButtonStyle} onClick={() => onClickDelete(i)}>Delete</button>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>{args.username}'s Song Explorer</h1>
+      {args.has_artists_saved ? (
+        <>
+          <h2>{args.song_name}</h2>
+          <h3>{args.song_artist}</h3>
+          <div>
+            <img src={args.song_image_url} width={300} height={300} />
+          </div>
+          <div>
+            <audio controls>
+              <source src={args.preview_url} />
+            </audio>
+          </div>
+          <a href={args.genius_url}> Click here to see lyrics! </a>
+          <h1>Your saved artists:</h1>
+          {artists_list}
+        </>
+      ) : (
+        <h2>Looks like you don't have anything saved! Use the form below!</h2>
+      )}
+      <h1>Save a favorite artist ID for later:</h1>
+      <input type="text" ref={form}></input>
+      <button onClick={onClickAdd}>Add Artist</button>
+      <button onClick={onClickSave}>Save</button>
+    </div>
+  );
 }
 
 export default App;
