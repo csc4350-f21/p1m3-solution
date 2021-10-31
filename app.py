52a0fb3eaa4c34ec706e3c7ecbedd49edfb9aff9
jomart-gsu <jmartin191@gsu.edu>
2021-10-31 18:07:59 -0400

    Python linting

--- app.py
+++ app.py
@@ -1,50 +1,74 @@
-import flask
-from flask_login.utils import login_required
-import requests
+"""
+Flask app logic for P1M3
+"""
+# pylint: disable=no-member
+# pylint: disable=too-few-public-methods
 import os
 import json
-
 import random
-import base64
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+import flask
+
+from dotenv import load_dotenv, find_dotenv
+from flask_login import (
+    login_user,
+    current_user,
+    LoginManager,
+    UserMixin,
+    login_required,
+)
+from flask_sqlalchemy import SQLAlchemy
 
 from genius import get_lyrics_link
 from spotify import get_access_token, get_song_data
 
-from flask_login import login_user, current_user, LoginManager
-from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__, static_folder="./build/static")
 # Point SQLAlchemy to your Heroku database
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 # Gets rid of a warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-from flask_login import UserMixin
+app.secret_key = b"I am a secret key"
 
 db = SQLAlchemy(app)
 
 
 class User(UserMixin, db.Model):
+    """
+    Model for a) User rows in the DB and b) Flask Login object
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
 
     def __repr__(self):
+        """
+        Determines what happens when we print an instance of the class
+        """
         return f"<User {self.username}>"
 
     def get_username(self):
+        """
+        Getter for username attribute
+        """
         return self.username
 
 
 class Artist(db.Model):
+    """
+    Model for saved artists
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
+        """
+        Determines what happens when we print an instance of the class
+        """
         return f"<Artist {self.artist_id}>"
 
 
@@ -56,23 +80,10 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_name):
+    """
+    Required by flask_login
+    """
     return User.query.get(user_name)
-
-
-def get_access_token():
-    auth = base64.standard_b64encode(
-        bytes(
-            f"{os.getenv('SPOTIFY_CLIENT_ID')}:{os.getenv('SPOTIFY_CLIENT_SECRET')}",
-            "utf-8",
-        )
-    ).decode("utf-8")
-    response = requests.post(
-        "https://accounts.spotify.com/api/token",
-        headers={"Authorization": f"Basic {auth}"},
-        data={"grant_type": "client_credentials"},
-    )
-    json_response = response.json()
-    return json_response["access_token"]
 
 
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
@@ -81,6 +92,10 @@ bp = flask.Blueprint("bp", __name__, template_folder="./build")
 @bp.route("/index")
 @login_required
 def index():
+    """
+    Main page. Fetches song data and embeds it in the returned HTML. Returns
+    dummy data if something goes wrong.
+    """
     artists = Artist.query.filter_by(username=current_user.username).all()
     artist_ids = [a.artist_id for a in artists]
     has_artists_saved = len(artist_ids) > 0
@@ -126,11 +141,17 @@ app.register_blueprint(bp)
 
 @app.route("/signup")
 def signup():
+    """
+    Signup endpoint for GET requests
+    """
     return flask.render_template("signup.html")
 
 
 @app.route("/signup", methods=["POST"])
 def signup_post():
+    """
+    Handler for signup form data
+    """
     username = flask.request.form.get("username")
     user = User.query.filter_by(username=username).first()
     if user:
@@ -145,23 +166,32 @@ def signup_post():
 
 @app.route("/login")
 def login():
+    """
+    Login endpoint for GET requests
+    """
     return flask.render_template("login.html")
 
 
 @app.route("/login", methods=["POST"])
 def login_post():
+    """
+    Handler for login form data
+    """
     username = flask.request.form.get("username")
     user = User.query.filter_by(username=username).first()
     if user:
         login_user(user)
         return flask.redirect(flask.url_for("bp.index"))
 
-    else:
-        return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
+    return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
 
 
 @app.route("/save", methods=["POST"])
 def save():
+    """
+    Receives JSON data from App.js, filters out invalid artist IDs, and
+    updates the DB to contain all valid ones and nothing else.
+    """
     artist_ids = flask.request.json.get("artist_ids")
     valid_ids = set()
     for artist_id in artist_ids:
@@ -169,16 +199,23 @@ def save():
             access_token = get_access_token()
             get_song_data(artist_id, access_token)
             valid_ids.add(artist_id)
-        except Exception:
+        except KeyError:
             pass
 
     username = current_user.username
+    update_db_ids_for_user(username, valid_ids)
 
     response = {"artist_ids": [a for a in artist_ids if a in valid_ids]}
     return flask.jsonify(response)
 
 
 def update_db_ids_for_user(username, valid_ids):
+    """
+    Updates the DB so that only entries for valid_ids exist in it.
+    @param username: the username of the current user
+    @param valid_ids: a set of artist IDs that the DB should update itself
+        to reflect
+    """
     existing_ids = {
         v.artist_id for v in Artist.query.filter_by(username=username).all()
     }
@@ -195,6 +232,10 @@ def update_db_ids_for_user(username, valid_ids):
 
 @app.route("/")
 def main():
+    """
+    Main page just reroutes to index or login depending on whether the
+    user is authenticated
+    """
     if current_user.is_authenticated:
         return flask.redirect(flask.url_for("bp.index"))
     return flask.redirect(flask.url_for("login"))
@@ -203,5 +244,5 @@ def main():
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8081)),
+        port=int(os.getenv("PORT", "8081")),
     )
